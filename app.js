05edda22d9cff070c5453c07225ebabd741e3554
Shivankkumar09 <shivank0912@gmail.com>
2024-07-17 11:51:00 +0530

    fixed sounds

--- app.js
+++ app.js
@@ -1,76 +1,84 @@
 const express = require("express");
-const socket  = require("socket.io");
+const socket = require("socket.io");
 const http = require("http");
 const { Chess } = require("chess.js");
-const { log } = require("console");
 const path = require("path");
 
 const app = express();
 
 const server = http.createServer(app);
-const io=  socket(server);
+const io = socket(server);
 
-const chess= new Chess();
-let player = {};
-let currentPlayer = "W";
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res )=>{
-    res.render("index", {title:"Chess Game"});
+app.get("/", (req, res) => {
+    res.render("index", { title: "Chess Game" });
 });
 
-io.on("connection", (uniquesocket)=>{
-console.log("connected");
-
-if(!player.white){
-    player.white = uniquesocket.id;
-    uniquesocket.emit("playerRole","w");
-}
-else if(!player.black){
-    player.black = uniquesocket.id;
-    uniquesocket.emit("playerRole","b");
-}
-else{
-    uniquesocket.emit("spectatorRole");
-}
-
-uniquesocket.on("disconnect", ()=>{
-    if(uniquesocket.id === player.white){
-  delete player.white;
-    }
-    else if(uniquesocket.id === player.black){
-        delete player.black;
-    }
-})
-
-
-uniquesocket.on("move", (move)=>{
-try {
-   if(chess.turn() === "w" && uniquesocket.id !== player.white) return;
-   if(chess.turn() === "b" && uniquesocket.id !== player.black) return;
-    
-   const result = chess.move(move);
-   if(result){
-    currentPlayer = chess.turn();
-    io.emit("move", move);
-    io.emit("boardState", chess.fen())
-   }
-   else{
-    console.log("Invalid move :", move);
-    uniquesocket.emit("invalidMove", move);
-   }
-
-} catch (err) {
-    console.log(err);
-    uniquesocket.emit("Invalid move :", move)
-}
-})
-
-
+app.get('/play', (req, res) => {
+    res.render('play'); 
 });
 
-server.listen(3000, function(){
+io.on("connection", (socket) => {
+    console.log("connected");
+
+    if (!players.white) {
+        players.white = socket.id;
+        socket.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = socket.id;
+        socket.emit("playerRole", "b");
+    } else {
+        socket.emit("spectatorRole");
+    }
+
+    socket.on("disconnect", () => {
+        if (socket.id === players.white) {
+            delete players.white;
+        } else if (socket.id === players.black) {
+            delete players.black;
+        }
+    });
+
+    socket.on("move", (move) => {
+        try {
+            if (chess.turn() === "w" && socket.id !== players.white) return;
+            if (chess.turn() === "b" && socket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if (result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+                
+                if (result.flags.includes("c")) {
+                    io.emit("capture");
+                } else {
+                    io.emit("selfMove");
+                }
+                
+                if (chess.isCheckmate()) {
+                    io.emit("notify");
+                }
+            } else {
+                io.emit("invalid");
+                console.log("Invalid move:", move);
+                socket.emit("invalidMove", move);
+               
+            }
+        } catch (err) {
+            io.emit("invalid");
+            console.log(err);
+            socket.emit("invalidMove", move);
+        }
+    });
+});
+
+server.listen(3000, function () {
     console.log("listening on 3000");
-})
+});
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -78,8 +78,32 @@ const handleMove = (source, target) => {
    };
 
    socket.emit("move" ,move);
-    };
+};
 
+const highlightPossibleMoves = (square) => {
+    clearHighlightedMoves();
+    const moves = chess.moves({ square: `${String.fromCharCode(97 + square.col)}${8 - square.row}`, verbose: true });
+    moves.forEach(move => {
+        const row = 8 - move.to.charCodeAt(1);
+        const col = move.to.charCodeAt(0) - 97;
+        const squareElement = document.querySelector(`.square[data-row='${row}'][data-col='${col}']`);
+        if (squareElement) {
+            const dot = document.createElement("div");
+            dot.classList.add("dot");
+            squareElement.classList.add("highlight");
+            squareElement.appendChild(dot);
+            possibleMoves.push(squareElement);
+        }
+    });
+};
+
+const clearHighlightedMoves = () => {
+    possibleMoves.forEach(square => {
+        square.classList.remove("highlight");
+        square.querySelector(".dot").remove();
+    });
+    possibleMoves = [];
+};
 
 const getPieceUnicode = (piece) => {
     const unicodePieces = {
@@ -99,24 +123,52 @@ const getPieceUnicode = (piece) => {
     return unicodePieces[piece.type] || "";
 };
 
-socket.on("playerRole", function (role){
+const updateTurnIndicator = () => {
+    const currentTurn = chess.turn() === 'w' ? 'White' : 'Black';
+    turnIndicator.innerText = `Current Turn: ${currentTurn}`;
+};
+
+socket.on("playerRole", function (role) {
     playerRole = role;
     renderBoard();
+    updateTurnIndicator();
 });
 
-socket.on("spectatorRole", function(){
+socket.on("spectatorRole", function () {
     playerRole = null;
     renderBoard();
-})
+    updateTurnIndicator();
+});
 
-socket.on("boardState", function(){
+
+
+socket.on("boardState", function(fen){
     chess.load(fen);
     renderBoard();
+    updateTurnIndicator();
 });
 
 socket.on("move", function(move){
     chess.move(move);
     renderBoard();
+    updateTurnIndicator();
+});
+
+socket.on("selfMove", () => {
+    document.getElementById("move-self").play();
+});
+
+socket.on("capture", () => {
+    document.getElementById("capture").play();
+});
+
+socket.on("notify", () => {
+    document.getElementById("notify").play();
+});
+
+socket.on("invalid", () => {
+    document.getElementById("invalid").play();
 });
 
 renderBoard();
+updateTurnIndicator();
